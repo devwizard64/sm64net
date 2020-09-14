18fdaeb24f3be6ca6abfd890af1bff706dca1ee7
devwizard <devwizard64@gmail.com>
2020-09-13 19:38:11 -0700

    add missing license info

---
+++ server/plugin/__init__.py
@@ -0,0 +1,4 @@
+#                   SM64Net - An Internet framework for SM64
+#                    Copyright (C) 2019, 2020  devwizard
+#       This project is licensed under the GNU General Public License
+#       version 2.  See LICENSE for more information.
