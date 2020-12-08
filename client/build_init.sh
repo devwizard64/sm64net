43ea55ca537b9087542ad7a376d13d286693b44f
devwizard <devwizard64@gmail.com>
2020-12-07 23:05:19 -0800

    clean up build_init.sh

--- client/build_init.sh
+++ client/build_init.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
 #                   SM64Net - An Internet framework for SM64
 #                    Copyright (C) 2019, 2020  devwizard
 #       This project is licensed under the GNU General Public License
 #       version 2.  See LICENSE for more information.
 
-set -e
 VERSION=1.1.0
 CC="$CC -Wall -Wextra -Wpedantic -I ../include -D _VERSION=\"$VERSION\""
 MKDIR()
